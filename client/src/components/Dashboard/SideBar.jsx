25fd490857fce52126d0b1f6f9e2aa239f5b8f9a
KariRomero <karinaromero.1993@gmail.com>
2024-07-31 10:27:19 -0300

    responsive sidebar

--- client/src/components/Dashboard/SideBar.jsx
+++ client/src/components/Dashboard/SideBar.jsx
@@ -1,40 +1,120 @@
+// import { Link, useLocation } from "react-router-dom";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faPaw, faRightFromBracket, faBars } from '@fortawesome/free-solid-svg-icons';
+// import { useState } from "react";
+
+// const SideBar = () => {
+
+//     const [showMenu, setShowMenu] = useState(false);
+
+//     const toggleMenu = () => {
+//         setShowMenu(!showMenu);
+//     };
+
+//     return (
+//         <aside className="fixed top-0 left-0 z-40 w-64 h-screen bg-primary">
+
+//             <FontAwesomeIcon icon={faBars} className="cursor-pointer" onClick={toggleMenu} />
+
+//             {showMenu && (
+//                 <ul className="space-y-2 font-medium">
+//                     <Link to={`/`}>
+//                         <li>
+//                             <button className="menu-btn border border-secondary rounded-full hover:bg-secondary ml-4">
+//                                 <FontAwesomeIcon icon={faPaw} className='mr-2' />
+//                                 Ver Sitio
+//                             </button>
+//                         </li>
+//                     </Link>
+//                     <NavLink to='/admin/rescates'>
+//                         Rescates
+//                     </NavLink>
+//                     <NavLink to='/admin/adopciones'>
+//                         Adopciones
+//                     </NavLink>
+//                     <NavLink to='/admin/socios'>
+//                         Socios
+//                     </NavLink>
+//                     <NavLink to='/admin/usuarios'>
+//                         Usuarios
+//                     </NavLink>
+//                     <NavLink to='/admin/cerrar-sesion'>
+//                         <FontAwesomeIcon icon={faRightFromBracket} className="mr-2" />
+//                         Cerrar sesión
+//                     </NavLink>
+//                 </ul>
+//             )}
+//         </aside>
+//     )
+// };
+
+// const NavLink = ({ to, children }) => {
+//     const location = useLocation();
+//     const isSelected = location.pathname.includes(to);
+
+//     return (
+//         <Link to={to} >
+//             <li className="hover:bg-secondary">
+//                 <button className={`menu-btn ${isSelected ? 'menu-selected' : 'hover:bg-secondary'}`}>
+//                     {children}
+//                 </button>
+
+//             </li>
+//         </Link>
+//     );
+// };
+
+// export default SideBar;
+
 import { Link, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPaw, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { faPaw, faRightFromBracket, faBars } from '@fortawesome/free-solid-svg-icons';
+import { useState } from "react";
 
 const SideBar = () => {
-    const location = useLocation();
-    const isSelected = (path) => location.pathname.includes(path);
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
 
     return (
-        <aside className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 bg-primary">
-            <ul className="space-y-2 font-medium">
-                <Link to={`/`}>
-                    <li>
-                        <button className="menu-btn border border-secondary rounded-full hover:bg-secondary ml-4">
-                            <FontAwesomeIcon icon={faPaw} className='mr-2' />
-                            Ver Sitio
-                        </button>
-                    </li>
-                </Link>
-                <NavLink to='/admin/rescates'>
-                    Rescates
-                </NavLink>
-                <NavLink to='/admin/adopciones'>
-                    Adopciones
-                </NavLink>
-                <NavLink to='/admin/socios'>
-                    Socios
-                </NavLink>
-                <NavLink to='/admin/usuarios'>
-                    Usuarios
-                </NavLink>
-                <NavLink to='/admin/cerrar-sesion'>
-                    <FontAwesomeIcon icon={faRightFromBracket} className="mr-2" />
-                    Cerrar sesión
-                </NavLink>
-            </ul>
-        </aside>
+        <>
+            <div className="fixed top-4 left-4 z-50 md:hidden">
+                <FontAwesomeIcon icon={faBars} className="cursor-pointer" onClick={toggleMenu} />
+            </div>
+            <aside className={`fixed top-0 left-0 z-40 h-screen w-64 bg-primary transition-transform transform ${showMenu ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0`}>
+                <div className="p-4 md:hidden">
+                    <FontAwesomeIcon icon={faBars} className="cursor-pointer" onClick={toggleMenu} />
+                </div>
+                <ul className="space-y-2 font-medium">
+                    <Link to={`/`}>
+                        <li>
+                            <button className="menu-btn border border-secondary rounded-full hover:bg-secondary ml-4">
+                                <FontAwesomeIcon icon={faPaw} className='mr-2' />
+                                Ver Sitio
+                            </button>
+                        </li>
+                    </Link>
+                    <NavLink to='/admin/rescates'>
+                        Rescates
+                    </NavLink>
+                    <NavLink to='/admin/adopciones'>
+                        Adopciones
+                    </NavLink>
+                    <NavLink to='/admin/socios'>
+                        Socios
+                    </NavLink>
+                    <NavLink to='/admin/usuarios'>
+                        Usuarios
+                    </NavLink>
+                    <NavLink to='/admin/cerrar-sesion'>
+                        <FontAwesomeIcon icon={faRightFromBracket} className="mr-2" />
+                        Cerrar sesión
+                    </NavLink>
+                </ul>
+            </aside>
+        </>
     )
 };
 
--- client/src/components/Footter/Footter.jsx
+++ client/src/components/Footter/Footter.jsx
@@ -6,13 +6,13 @@ import { Link } from "react-router-dom";
 
 const Footter = ()=>{
     return(
-        <div className="h-48 sm:h-96 grid grid-cols-2 border border-t-grey border-b-0 border-x-0 ">
-            <div className="h-36 sm:h-72 m-auto">
+        <div className="grid grid-cols-2 border border-t-grey border-b-0 border-x-0">
+            <div className="h-36 md:h-72 m-auto">
                 <Link to='/'>
                     <Logo/>
                 </Link>
             </div>
-            <div className="mt-24 pb-16">
+            <div className="mt-24 pb-16 mr-2">
                 <h1 className='text-secondary text-base font-medium tracking-wider'>Adoptá un Callejerito</h1>
                 <Link to='/'>
                     <p className="text-base font-normal tracking-wide my-2 hover:text-secondary">Inicio</p>
@@ -31,7 +31,7 @@ const Footter = ()=>{
                     <p className="text-base font-normal tracking-wide my-2 hover:text-secondary">Administrador</p>
                 </Link>   
                 <a href='https://www.instagram.com/fundacion.callejeritos/' target='_blank' className="hover:text-secondary">
-                    Encontranos en Instagram<FontAwesomeIcon icon={faInstagram} size="xl" className=' ml-2' />
+                    En Instagram<FontAwesomeIcon icon={faInstagram} size="xl" className=' ml-2' />
                 </a> 
             </div>
         </div>
