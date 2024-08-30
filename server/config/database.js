b51b32de81a380dbc521bf6d04ce9317bbc6520d
franravera <fraaanco.10@hotmail.com>
2024-08-30 14:25:03 -0300

    rama deploy lista

--- server/config/database.js
+++ server/config/database.js
@@ -2,10 +2,15 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  logging: false,
-});
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+//   host: process.env.DB_HOST,
+//   dialect: 'postgres',
+//   logging: false,
+// });
 
+
+const sequelize = new Sequelize(
+  process.env.DB_DEPLOY,
+  { logging: false, native: false }
+);
 module.exports = sequelize;
