c8fe9deae2bb226d9c36b9cff574b16d00d553d9
franravera <fraaanco.10@hotmail.com>
2024-08-31 16:12:48 -0300

    no sse22

--- client/src/redux/auth/authActions.js
+++ client/src/redux/auth/authActions.js
@@ -3,30 +3,14 @@ import { logInGoogle, getCurrentUser, logOutGoogle } from './authSlice';
 
 // Inicia el proceso de autenticación con Google
 export const startGoogleLogin = () => async (dispatch) => {
-  window.location.href = 'https://fundacioncallejeritos-production.up.railway.app/auth/google';
+  // Usa una ruta relativa para la autenticación cuando se utiliza un proxy inverso
+  window.location.href = '/auth/google';
 };
 
 // Obtiene el usuario actual autenticado
-
-
-// export const fetchCurrentUser = () => async (dispatch) => {
-//   try {
-//     const response = await axios.get('https://fundacioncallejeritos-production.up.railway.app/current_user', { withCredentials: true });
-
-//     if (response.data) {
-//       dispatch(getCurrentUser(response.data)); // Aquí se despacha la acción para actualizar el estado
-//     } else {
-//       dispatch(logOutGoogle()); // Si no hay datos de usuario, se despacha la acción para marcar como no autenticado
-//     }
-//   } catch (error) {
-//     console.error("Fetching current user failed:", error);
-//     dispatch(logOutGoogle()); // Si ocurre un error, se despacha la acción para marcar como no autenticado
-//   }
-// };
-
 export const fetchCurrentUser = () => async (dispatch) => {
   try {
-    const response = await axios.get('https://fundacioncallejeritos-production.up.railway.app/current_user', { withCredentials: true });
+    const response = await axios.get('/auth/current_user', { withCredentials: true }); // Usa la ruta relativa
 
     console.log("Response Headers:", response.headers);
     console.log("Response Data:", response.data);
@@ -46,10 +30,11 @@ export const fetchCurrentUser = () => async (dispatch) => {
     dispatch(logOutGoogle());
   }
 };
+
 // Cierra sesión
 export const startGoogleLogout = () => async (dispatch) => {
   try {
-    const response = await axios.get('https://fundacioncallejeritos-production.up.railway.app/logout', { withCredentials: true });
+    const response = await axios.get('/auth/logout', { withCredentials: true });  // Usa la ruta relativa
     console.log('Respuesta de logout:', response);
     dispatch(logOutGoogle());
   } catch (error) {
