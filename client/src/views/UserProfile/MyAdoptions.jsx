08b3e644830db9ac1d95388a7926915fda33a8e3
KariRomero <karinaromero.1993@gmail.com>
2024-09-03 18:16:30 -0300

    MyAdoptions prueba rsponsive

--- client/src/views/UserProfile/MyAdoptions.jsx
+++ client/src/views/UserProfile/MyAdoptions.jsx
@@ -24,8 +24,8 @@ const MyAdoptions = () => {
     };
 
     return (
-        <section className="flex h-screen justify-center px-4">
-            <ul className="w-full grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 mt-4">
+        <section className="w-full h-screen px-4">
+            <ul className="w-full mt-4">
                 {Array.isArray(adopcionesList) && adopcionesList.map(adop => (
                     <CardAdoptions
                         key={adop.id}
