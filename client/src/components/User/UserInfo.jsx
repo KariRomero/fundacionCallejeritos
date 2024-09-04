e308d301370c3827cd689cf6d5c597dceafd62b5
KariRomero <karinaromero.1993@gmail.com>
2024-09-04 10:12:53 -0300

    nueva disposicion UserInfo

--- client/src/components/User/UserInfo.jsx
+++ client/src/components/User/UserInfo.jsx
@@ -10,14 +10,14 @@ const UserInfo = () => {
     const navigate = useNavigate();
     const { id } = useParams();
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(getById(id))
-    },[dispatch,id])
+    }, [dispatch, id])
 
     const { user } = useSelector(state => state.users);
 
     const birthDate = user.birthDate === 'Invalid date' ? '' : user.birthDate;
-    
+
 
     const [form, setForm] = useState({
         birthDate: '',
@@ -33,7 +33,7 @@ const UserInfo = () => {
         email: '',
         image: ''
     })
-    
+
 
     useEffect(() => {
         if (user) {
@@ -74,147 +74,120 @@ const UserInfo = () => {
     };
 
     return (
-        <section className='w-full h-screen mt-12'>
-            <form onSubmit={handleSubmit} className='w-full sm:w-1/2 md:w-1/2 mx-auto '>
-                <div className="my-2 flex justify-between items-center">
-                    <div className='grid grid-cols-1'>
-                        <label className='label'>Nombre:</label>
-                        <input
-                            type="text"
-                            className='rounded-full shadow-md p-2'
-                            value={form.firstName}
-                            name='firstName'
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className='grid grid-cols-1'>
-                        <label className='label'>Apellido:</label>
-                        <input
-                            type="text"
-                            className='rounded-full shadow-md p-2'
-                            value={form.lastName}
-                            name='lastName'
-                            onChange={handleChange}
-                        />
-                    </div>
+        <section className='w-full h-screen mt-12 grid grid-cols-1'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2'>
+                <img src={user.image} alt='Foto de perfil' className="w-40 object-cover rounded-t-lg transition-transform transform hover:scale-110" />
+                <div>
+                    <h1>Bienvenido, {user.firstName} {user.lastName}</h1>
+                    <span>{user.email}</span>
                 </div>
-
-                <div className="my-2 flex justify-between items-center">
-                    <div className='grid grid-cols-1'>
-                        <label className='label'>Teléfono:</label>
-                        <input
-                            placeholder='Escriba su teléfono'
-                            type="text"
-                            className='rounded-full shadow-md p-2'
-                            value={form.phone}
-                            name='phone'
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className='grid grid-cols-1'>
-                        <label className='label'>Celular:</label>
-                        <input
-                            placeholder='Su celular sin 0 y sin 15'
-                            type="text"
-                            className='rounded-full shadow-md p-2'
-                            value={form.mobile}
-                            name='mobile'
-                            onChange={handleChange}
-                        />
-                    </div>
-                </div>
-
-                <div className="my-2 flex justify-between items-center">
-                    <div className='grid grid-cols-1'>
-                        <label className='label'>Provincia:</label>
-                        <input
-                            placeholder='Escriba su provincia'
-                            type="text"
-                            className='rounded-full shadow-md p-2'
-                            value={form.state}
-                            name='state'
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className='grid grid-cols-1'>
-                        <label className='label'>Ciudad:</label>
-                        <input
-                            placeholder='Escriba su ciudad'
-                            type="text"
-                            className='rounded-full shadow-md p-2'
-                            value={form.city}
-                            name='city'
-                            onChange={handleChange}
-                        />
+            </div>
+            <div>
+                <form onSubmit={handleSubmit} className='w-full sm:w-1/2 md:w-1/2 mx-auto '>
+                    <div className="my-2 flex justify-between items-center">
+                        <div className='grid grid-cols-1'>
+                            <label className='label'>Teléfono:</label>
+                            <input
+                                placeholder='Escriba su teléfono'
+                                type="text"
+                                className='rounded-full shadow-md p-2'
+                                value={form.phone}
+                                name='phone'
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className='grid grid-cols-1'>
+                            <label className='label'>Celular:</label>
+                            <input
+                                placeholder='Su celular sin 0 y sin 15'
+                                type="text"
+                                className='rounded-full shadow-md p-2'
+                                value={form.mobile}
+                                name='mobile'
+                                onChange={handleChange}
+                            />
+                        </div>
                     </div>
 
-                </div>
-                
-                <div className="my-2 flex justify-between items-center">
-                    <div className='grid grid-cols-1'>
-                        <label className='label'>Direccion:</label>
-                        <input
-                            placeholder='Avenida Ramirez 1234'
-                            type="text"
-                            className='rounded-full shadow-md p-2'
-                            value={form.address}
-                            name='address'
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className='grid grid-cols-1'>
-                        <label className='label'>C.P:</label>
-                        <input
-                            placeholder='3100'
-                            type="text"
-                            className='rounded-full shadow-md p-2'
-                            value={form.postalCode}
-                            name='postalCode'
-                            onChange={handleChange}
-                        />
-                    </div>
-                </div>
+                    <div className="my-2 flex justify-between items-center">
+                        <div className='grid grid-cols-1'>
+                            <label className='label'>Provincia:</label>
+                            <input
+                                placeholder='Escriba su provincia'
+                                type="text"
+                                className='rounded-full shadow-md p-2'
+                                value={form.state}
+                                name='state'
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className='grid grid-cols-1'>
+                            <label className='label'>Ciudad:</label>
+                            <input
+                                placeholder='Escriba su ciudad'
+                                type="text"
+                                className='rounded-full shadow-md p-2'
+                                value={form.city}
+                                name='city'
+                                onChange={handleChange}
+                            />
+                        </div>
 
-
-                <div className="my-2 flex justify-between items-center">
-                    <div className='grid grid-cols-1'>
-                        <label className='label'>DNI:</label>
-                        <input
-                            placeholder='Escriba su dni'
-                            type="text"
-                            className='rounded-full shadow-md p-2'
-                            value={form.dni}
-                            name='dni'
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className='grid grid-cols-1'>
-                        <label className='label'>E-mail:</label>
-                        <input
-                            placeholder='Su celular sin 0 y sin 15'
-                            type="text"
-                            className='rounded-full shadow-md p-2'
-                            value={form.email}
-                            name='email'
-                            onChange={handleChange}
-                        />
-                    </div>                    
-                </div>
-
-                <div className='grid grid-cols-1'>
-                        <label className='label'>Fecha de nacimiento:</label>
-                        <input
-                            placeholder='DD/MM/YYYY'
-                            type="text"
-                            className='rounded-full shadow-md p-2'
-                            value={form.birthDate}
-                            name='birthDate'
-                            onChange={handleChange}
-                        />
                     </div>
 
-                <button type="submit" className="menu-btn border border-secondary rounded-full hover:bg-secondary">Guardar</button>
-            </form>
+                    <div className="my-2 flex justify-between items-center">
+                        <div className='grid grid-cols-1'>
+                            <label className='label'>Direccion:</label>
+                            <input
+                                placeholder='Avenida Ramirez 1234'
+                                type="text"
+                                className='rounded-full shadow-md p-2'
+                                value={form.address}
+                                name='address'
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className='grid grid-cols-1'>
+                            <label className='label'>C.P:</label>
+                            <input
+                                placeholder='3100'
+                                type="text"
+                                className='rounded-full shadow-md p-2'
+                                value={form.postalCode}
+                                name='postalCode'
+                                onChange={handleChange}
+                            />
+                        </div>
+                    </div>
+
+
+                    <div className="my-2 flex justify-between items-center">
+                        <div className='grid grid-cols-1'>
+                            <label className='label'>DNI:</label>
+                            <input
+                                placeholder='Escriba su dni'
+                                type="text"
+                                className='rounded-full shadow-md p-2'
+                                value={form.dni}
+                                name='dni'
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className='grid grid-cols-1'>
+                            <label className='label'>Fecha de nacimiento:</label>
+                            <input
+                                placeholder='DD/MM/YYYY'
+                                type="text"
+                                className='rounded-full shadow-md p-2'
+                                value={form.birthDate}
+                                name='birthDate'
+                                onChange={handleChange}
+                            />
+                        </div>
+                    </div>
+                    <button type="submit" className="menu-btn border border-secondary rounded-full hover:bg-secondary">Guardar</button>
+                </form>
+            </div>
         </section>
     )
 };
