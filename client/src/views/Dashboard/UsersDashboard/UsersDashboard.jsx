37d0348e8b3c340f6651a73bfb29c55d49a48992
KariRomero <karinaromero.1993@gmail.com>
2024-09-04 11:23:43 -0300

    estilos UsersDashboard

--- client/src/views/Dashboard/UsersDashboard/UsersDashboard.jsx
+++ client/src/views/Dashboard/UsersDashboard/UsersDashboard.jsx
@@ -31,13 +31,13 @@ const UsersDashboard = () => {
                 <ul className="w-full">
                     {Array.isArray(users) && users.map((user) => (
                         <li key={user.id} className="flex justify-between items-center border border-l-0 border-r-0 border-t-0 border-b-secondary p-4">
-                            <div className='flex'>
+                            <div className='flex items-center'>
                                 <img src={user.image} alt='foto perfil' className='w-16 h-1w-16 rounded-full mx-4' />
                                 <h2 className="font-normal tracking-wide text-sm">{user.firstName} {user.lastName}</h2>
                             </div>
                             <div>
                                 <Link to={`/admin/usuarios/user/${user.id}`}>
-                                    <button className='font-medium text-base tracking-wider p-2 border border-secondary rounded-full hover:bg-secondary'>
+                                    <button className='font-medium text-base tracking-wider p-2 mx-2'>
                                         <FontAwesomeIcon icon={faEye}/>                                        
                                     </button>
                                 </Link>
