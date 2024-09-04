aa4dd81e252efd943419213dbaf0ac1fb1c5f658
KariRomero <karinaromero.1993@gmail.com>
2024-09-04 10:15:11 -0300

    estilos  UserInfo

--- client/src/components/User/UserInfo.jsx
+++ client/src/components/User/UserInfo.jsx
@@ -75,11 +75,11 @@ const UserInfo = () => {
 
     return (
         <section className='w-full h-screen mt-12 grid grid-cols-1'>
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 mx-auto gap-2'>
                 <img src={user.image} alt='Foto de perfil' className="w-40 object-cover rounded-t-lg transition-transform transform hover:scale-110" />
                 <div>
-                    <h1>Bienvenido, {user.firstName} {user.lastName}</h1>
-                    <span>{user.email}</span>
+                    <h1 className='title'>Bienvenido, {user.firstName} {user.lastName}</h1>
+                    <span className='paragraph'>{user.email}</span>
                 </div>
             </div>
             <div>
